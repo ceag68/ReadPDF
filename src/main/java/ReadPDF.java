b51434806b4ce63020c326c02efd713dec5941c1
ceag68 <ceag9498@gmail.com>
2022-04-01 12:15:15 -0500

    Fixing Regex expressions and adding date period scanner.

--- src/main/java/ReadPDF.java
+++ src/main/java/ReadPDF.java
@@ -18,8 +18,9 @@ public class ReadPDF {
         System.out.println("Read PDF starts here ...");
 
         try {
-            String filePath = "C:\\Users\\ceag9\\OneDrive\\Documents\\CEAG\\Personal\\Financial\\BankStatements\\2021\\Chase\\CEA-CC-4540\\20210607-statements-4540-.pdf";
+//            String filePath = "C:\\Users\\ceag9\\OneDrive\\Documents\\CEAG\\Personal\\Financial\\BankStatements\\2021\\Chase\\CEA-CC-4540\\20210607-statements-4540-.pdf";
 //            String filePath = "C:\\Users\\ceag9\\OneDrive\\Documents\\CEAG\\Personal\\Financial\\BankStatements\\2021\\Chase\\CEA-CC-4540\\20210507-statements-4540-.pdf";
+            String filePath = "/Users/carlosalvarez/Downloads/20220107-statements-4540-.pdf";
             PDDocument document = PDDocument.load(new File(filePath));
 
             //if (!document.isEncrypted()) {
@@ -29,9 +30,8 @@ public class ReadPDF {
             String[] lines = text.split(System.lineSeparator());
             int i = 1;
             for (String line: lines) {
-
-                //System.out.println("Line[" + i++ + "] : [" + line + "]");
-                processLine(line, i++);
+//              System.out.println("Line[" + i++ + "] : [" + line + "]");
+              processLine(line, i++);
             }
             System.out.println("Begin Balance: " + beginBalance);
             System.out.println("Total Payments: " + totalPayment);
@@ -49,8 +49,9 @@ public class ReadPDF {
     }
 
     public static void processLine(String line, int index) {
-        getPreviousBalance(line, index);
         getAccountNumber(line, index);
+        getPreviousBalance(line, index);
+        getDatePeriod(line, index);
         getPayment(line, index);
         getPurchase(line, index);
     }
@@ -60,36 +61,47 @@ public class ReadPDF {
         Pattern r = Pattern.compile(accountNumberRegex);
         Matcher m = r.matcher(line);
         if (m.find( )) {
-            System.out.println("Line[" + index + "] : Account Number: " + m.group(3));
+            System.out.println("Line[" + index + "] : Account Number: [" + m.group(3) + "]");
         }
     }
 
     public static void getPreviousBalance(String line, int index) {
-        String paymentRegex = "^(Previous\\s+Balance)(\\s+)\\$?(?<amount>(\\d+\\.\\d+)$)";
+        String paymentRegex = "^(Previous\\s+Balance)(\\s+)\\$?(?<amount>(\\d*\\,{0,1}\\d+\\.\\d+)$)";
         Pattern r = Pattern.compile(paymentRegex);
         Matcher m = r.matcher(line);
         if (m.find( )) {
-            beginBalance = beginBalance.add(new BigDecimal(m.group(3)));
-            System.out.println("Line[" + index + "] : Previous Balance: $" + m.group(3) + "]");
+            beginBalance = beginBalance.add(new BigDecimal(m.group(3).replaceAll(",", "")));
+            System.out.println("Line[" + index + "] : Previous Balance: amount=[" + m.group(3) + "]");
+        }
+    }
+
+
+    public static void getDatePeriod(String line, int index) {
+        String paymentRegex = "^(Opening\\/Closing\\s+Date)(\\s+)\\$?(?<opening>(\\d+\\/\\d+\\/\\d+))\\s*\\-\\s*\\$?(?<closing>(\\d+\\/\\d+\\/\\d+))$";
+        Pattern r = Pattern.compile(paymentRegex);
+        Matcher m = r.matcher(line);
+        if (m.find( )) {
+//            beginBalance = beginBalance.add(new BigDecimal(m.group(3).replaceAll(",", "")));
+            System.out.println("Line[" + index + "] : Date Period: opening=[" + m.group(4) + "] closing=[" + m.group(6) + "]");
         }
     }
 
     public static void getPayment(String line, int index) {
-        String paymentRegex = "(?<mm>^\\d{2})\\/(?<dd>\\d{2})(\\s+)(?<description>(.)+)(\\s+)(\\-)(?<amount>(\\d+\\.\\d+)$)";
+        String paymentRegex = "(?<mm>^\\d{2})\\/(?<dd>\\d{2})(\\s+)(?<description>(.)+)(\\s+)(\\-)(?<amount>(\\d*\\,{0,1}\\d*\\.\\d+)$)";
         Pattern r = Pattern.compile(paymentRegex);
         Matcher m = r.matcher(line);
         if (m.find( )) {
-            totalPayment = totalPayment.add(new BigDecimal(m.group(8)));
+            totalPayment = totalPayment.add(new BigDecimal(m.group(8).replaceAll(",", "")));
             System.out.println("Line[" + index + "] : Payment: " + m.group(1) + "/" + m.group(2) + " Description: [" + m.group(4) + "] amount=[" + m.group(8) + "]");
         }
     }
 
     public static void getPurchase(String line, int index) {
-        String paymentRegex = "(?<mm>^\\d{2})\\/(?<dd>\\d{2})(\\s+)(?<description>(.)+)(\\s+)(?<amount>(\\d*\\.\\d+)$)";
+        String paymentRegex = "(?<mm>^\\d{2})\\/(?<dd>\\d{2})(\\s+)(?<description>(.)+)(\\s+)(?<amount>(\\d*\\,{0,1}\\d*\\.\\d+)$)";
         Pattern r = Pattern.compile(paymentRegex);
         Matcher m = r.matcher(line);
         if (m.find( )) {
-            totalPurchase = totalPurchase.add(new BigDecimal(m.group(7)));
+            totalPurchase = totalPurchase.add(new BigDecimal(m.group(7).replaceAll(",", "")));
             System.out.println("Line[" + index + "] : Purchase: " + m.group(1) + "/" + m.group(2) + " Description: [" + m.group(4) + "] amount=[" + m.group(7) + "]");
         }
     }
